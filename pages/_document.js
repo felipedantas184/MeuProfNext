715925996aa92b13d3d216cf1467c774fb108ac1
Felipe <felipedantas184@gmail.com>
2021-09-30 23:42:14 -0300

    GA tag

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,22 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-8QGV56W7EB"
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-8QGV56W7EB', { page_path: window.location.pathname });
+              `,
+            }}
+          />
+
           <meta charSet="utf-8" />
           <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
@@ -47,6 +63,9 @@ export default class MyDocument extends Document {
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         </Head>
         <body>
+          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-8QGV56W7EB"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+
           <Main />
           <NextScript />
         </body>
