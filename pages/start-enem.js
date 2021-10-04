4107db074f0d4cef340cc13c85c0d7e216ba8a3c
Felipe <felipedantas184@gmail.com>
2021-10-04 10:27:18 -0300

    description

--- pages/start-enem.js
+++ pages/start-enem.js
@@ -8,20 +8,20 @@ export default function HowItWorks() {
   return (
     <>
       <Head>
-        <title>START ENEM - MeuProf | Aulas Particulares</title>
+        <title>START ENEM - MeuProf | SUPER REVISÃO + MENTORIA</title>
         <meta name="description" content="SUPER REVISÃO + MENTORIA Meu Prof! Nós organizamos um projeto especial de revisão para vocês ficarem ainda mais seguros nessa reta final!" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 
 
-        <meta property="og:title" content="Como Funciona - MeuProf | Aulas Particulares"/>
+        <meta property="og:title" content="START ENEM - MeuProf | SUPER REVISÃO + MENTORIA"/>
         <meta property="og:type" content="school"/>
-        <meta property="og:description" content="Encontrar o professor particular ideal para você nunca foi tão fácil! Confira o tutorial de como utilizar a plataforma MeuProf."/>
+        <meta property="og:description" content="SUPER REVISÃO + MENTORIA Meu Prof! Nós organizamos um projeto especial de revisão para vocês ficarem ainda mais seguros nessa reta final!"/>
         <meta property="og:image" content="/apple-touch-icon.png"/>
         <meta property="og:site_name" content="MeuProf"/>
 
-        <meta property="twitter:title" content="Como Funciona - MeuProf | Aulas Particulares"/>
+        <meta property="twitter:title" content="START ENEM - MeuProf | SUPER REVISÃO + MENTORIA"/>
         <meta property="twitter:url" content="https://meu-prof-next.vercel.app/como-funciona/"/>
-        <meta property="twitter:description" content="Encontrar o professor particular ideal para você nunca foi tão fácil! Confira o tutorial de como utilizar a plataforma MeuProf."/>
+        <meta property="twitter:description" content="SUPER REVISÃO + MENTORIA Meu Prof! Nós organizamos um projeto especial de revisão para vocês ficarem ainda mais seguros nessa reta final!"/>
         <meta property="twitter:image" content="/apple-touch-icon.png"/>
       </Head>
 
