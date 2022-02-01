8f939eacbdb368839de782425494abd53f88c572
Felipe <felipedantas184@gmail.com>
2022-02-01 10:18:14 -0300

    mentoria page title

--- pages/mentoria.js
+++ pages/mentoria.js
@@ -13,17 +13,17 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>MENTORIA ENEM - MeuProf | Aulas Particulares & Enem</title>
+        <title>Mentoria Enem - MeuProf | Aulas Particulares & Enem</title>
         <meta name="description" content="Acompanhamento individual e preparação o Enem com a eficiência e qualidade de quem já trilhou esse caminho!" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         
-        <meta property="og:title" content="MENTORIA ENEM - MeuProf | Aulas Particulares & Enem"/>
+        <meta property="og:title" content="Mentoria Enem - MeuProf | Aulas Particulares & Enem"/>
         <meta property="og:type" content="school"/>
         <meta property="og:description" content="Acompanhamento individual e preparação o Enem com a eficiência e qualidade de quem já trilhou esse caminho!"/>
         <meta property="og:image" content="/apple-touch-icon.png"/>
         <meta property="og:site_name" content="MeuProf"/>
 
-        <meta property="twitter:title" content="MENTORIA ENEM - MeuProf | Aulas Particulares & Enem"/>
+        <meta property="twitter:title" content="Mentoria Enem - MeuProf | Aulas Particulares & Enem"/>
         <meta property="twitter:url" content="https://meuprof.app.br/mentoria/"/>
         <meta property="twitter:description" content="Acompanhamento individual e preparação o Enem com a eficiência e qualidade de quem já trilhou esse caminho!"/>
         <meta property="twitter:image" content="/apple-touch-icon.png"/>
